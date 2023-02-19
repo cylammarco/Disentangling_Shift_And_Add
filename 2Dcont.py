43032e17dce186854ac27ebe6923e9de4f7f5c00
cylammarco <cylammarco@gmail.com>
2023-02-19 14:24:04 +0200

    tidied code a little bit

--- 2Dcont.py
+++ 2Dcont.py
@@ -5,13 +5,12 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
 
-
 ###########################
 ######### USER INPUT ######
 ###########################
 
 # Cut part of the chi2 matrix?
-indK1cut1, indK1cut2  = 0, -1
+indK1cut1, indK1cut2 = 0, -1
 indK2cut1, indK2cut2 = 0, -1
 
 
@@ -20,15 +19,15 @@ step1 = 3
 step2 = 3
 
 # File to be read
-fileK1K2 = 'Output/HeI4472_grid_dis_K1K2.txt'
-#fileK1K2 = 'HeII4546_grid_dis_K1K2.txt'
-#fileK1K2 = 'HeII4200_grid_dis_K1K2.txt'
-#fileK1K2 = 'HeI4388_grid_dis_K1K2.txt'
-#fileK1K2 = 'HeI4026_grid_dis_K1K2.txt'
-#fileK1K2 = 'HeI4009_grid_dis_K1K2.txt'
-#fileK1K2 = 'HeI4144_grid_dis_K1K2.txt'
-#fileK1K2 = 'Hdelta_grid_dis_K1K2.txt'
-#fileK1K2 = 'Indiv_grid_dis_K1K2.txt'
+fileK1K2 = "Output/HeI4472_grid_dis_K1K2.txt"
+# fileK1K2 = 'HeII4546_grid_dis_K1K2.txt'
+# fileK1K2 = 'HeII4200_grid_dis_K1K2.txt'
+# fileK1K2 = 'HeI4388_grid_dis_K1K2.txt'
+# fileK1K2 = 'HeI4026_grid_dis_K1K2.txt'
+# fileK1K2 = 'HeI4009_grid_dis_K1K2.txt'
+# fileK1K2 = 'HeI4144_grid_dis_K1K2.txt'
+# fileK1K2 = 'Hdelta_grid_dis_K1K2.txt'
+# fileK1K2 = 'Indiv_grid_dis_K1K2.txt'
 
 
 ###########################
@@ -38,76 +37,104 @@ fileK1K2 = 'Output/HeI4472_grid_dis_K1K2.txt'
 
 Z = np.loadtxt(fileK1K2)
 
-#MinInds =  np.where(Z == np.min(Z))
+# MinInds =  np.where(Z == np.min(Z))
 
 try:
-    fileK1 = fileK1K2[:-8] + 'K1.txt'
+    fileK1 = fileK1K2[:-8] + "K1.txt"
     sigma1 = float((open(fileK1).readlines())[0].split()[-1])
     K1s, chis1 = np.loadtxt(fileK1, unpack=True)
     minind1 = np.argmin(chis1)
-except: 
-    sigma1=0
+except:
+    sigma1 = 0
     K1s = [0]
     pass
 try:
-    fileK2 = fileK1K2[:-8] + 'K2.txt'
+    fileK2 = fileK1K2[:-8] + "K2.txt"
     sigma2 = float((open(fileK2).readlines())[0].split()[-1])
-    K2s, chis2 = np.loadtxt(fileK2, unpack=True)   
+    K2s, chis2 = np.loadtxt(fileK2, unpack=True)
     minind2 = np.argmin(chis2)
 except:
     K2s = [0]
-    sigma2=0
+    sigma2 = 0
     pass
-sigma = (sigma1 + sigma2)/2.
-
-
+sigma = (sigma1 + sigma2) / 2.0
 
 
 if len(K1s) > 1:
-    a,b,c = np.polyfit(K1s[max(minind1-step1, 0): min(minind1+step1, len(K1s)-1)], chis1[max(minind1-step1, 0): min(minind1+step1, len(K1s)-1)] , 2)    
-    Chi2K1min = -b/2./a
+    a, b, c = np.polyfit(
+        K1s[max(minind1 - step1, 0) : min(minind1 + step1, len(K1s) - 1)],
+        chis1[max(minind1 - step1, 0) : min(minind1 + step1, len(K1s) - 1)],
+        2,
+    )
+    Chi2K1min = -b / 2.0 / a
 
-    K1fine = np.arange(-K1s[-1], K1s[-1]+1, 0.01)
+    K1fine = np.arange(-K1s[-1], K1s[-1] + 1, 0.01)
 
-    parb = a*K1fine**2 + b*K1fine + c
+    parb = a * K1fine**2 + b * K1fine + c
 
     LeftPart = K1fine < Chi2K1min
-    K1err = Chi2K1min - K1fine[np.argmin((parb[LeftPart] - sigma1)**2)]
+    K1err = Chi2K1min - K1fine[np.argmin((parb[LeftPart] - sigma1) ** 2)]
 
     plt.scatter(K1s, chis1)
     plt.plot(K1fine, parb)
-    plt.plot([min(K1s), max(K1s)], [sigma1, sigma1], color='red', label='1-sigma')
-    plt.ylabel('reduced chi2')
-    plt.xlabel('K1 [km/s]')
-    plt.xlim(K1s[0]-1, K1s[-1] + 1)
-    plt.ylim(min(chis1)*.95, max(chis1)*1.05)
-    plt.text(np.average(K1s), max(chis1), r"$K_1 =$ " + str(round(Chi2K1min,1)) + r'$\pm$' + str(round(K1err,1)) +  " km/s", size=13, horizontalalignment='center')
+    plt.plot(
+        [min(K1s), max(K1s)], [sigma1, sigma1], color="red", label="1-sigma"
+    )
+    plt.ylabel("reduced chi2")
+    plt.xlabel("K1 [km/s]")
+    plt.xlim(K1s[0] - 1, K1s[-1] + 1)
+    plt.ylim(min(chis1) * 0.95, max(chis1) * 1.05)
+    plt.text(
+        np.average(K1s),
+        max(chis1),
+        r"$K_1 =$ "
+        + str(round(Chi2K1min, 1))
+        + r"$\pm$"
+        + str(round(K1err, 1))
+        + " km/s",
+        size=13,
+        horizontalalignment="center",
+    )
     plt.show()
-    print("K1: ", Chi2K1min, " +- " , K1err)
+    print("K1: ", Chi2K1min, " +- ", K1err)
 
 if len(K2s) > 1:
-    a,b,c = np.polyfit(K2s[max(minind2-step2, 0): min(minind2+step2, len(K2s)-1)], chis2[max(minind2-step2, 0): min(minind2+step2, len(K2s)-1)] , 2)    
-    Chi2K2min = -b/2./a
+    a, b, c = np.polyfit(
+        K2s[max(minind2 - step2, 0) : min(minind2 + step2, len(K2s) - 1)],
+        chis2[max(minind2 - step2, 0) : min(minind2 + step2, len(K2s) - 1)],
+        2,
+    )
+    Chi2K2min = -b / 2.0 / a
 
-    K2fine = np.arange(-K2s[-1], K2s[-1]+1, 0.01)
+    K2fine = np.arange(-K2s[-1], K2s[-1] + 1, 0.01)
 
-    parb = a*K2fine**2 + b*K2fine + c
+    parb = a * K2fine**2 + b * K2fine + c
 
     LeftPart = K2fine < Chi2K2min
-    K2err = Chi2K2min - K2fine[np.argmin((parb[LeftPart] - sigma2)**2)]
+    K2err = Chi2K2min - K2fine[np.argmin((parb[LeftPart] - sigma2) ** 2)]
 
     plt.scatter(K2s, chis2)
     plt.plot(K2fine, parb)
-    plt.plot([min(K2s), max(K2s)], [sigma2, sigma2], color='red', label='1-sigma')
-    plt.ylabel('reduced chi2')
-    plt.xlabel('K2 [km/s]')
-    plt.xlim(K2s[0]-1, K2s[-1] + 1)
-    plt.ylim(min(chis2)*.95, max(chis2)*1.05)
-    plt.text(np.average(K2s), max(chis2), r"$K_2 =$ " + str(round(Chi2K2min,1)) + r'$\pm$' + str(round(K2err,1)) +  " km/s", size=13, horizontalalignment='center')    
+    plt.plot(
+        [min(K2s), max(K2s)], [sigma2, sigma2], color="red", label="1-sigma"
+    )
+    plt.ylabel("reduced chi2")
+    plt.xlabel("K2 [km/s]")
+    plt.xlim(K2s[0] - 1, K2s[-1] + 1)
+    plt.ylim(min(chis2) * 0.95, max(chis2) * 1.05)
+    plt.text(
+        np.average(K2s),
+        max(chis2),
+        r"$K_2 =$ "
+        + str(round(Chi2K2min, 1))
+        + r"$\pm$"
+        + str(round(K2err, 1))
+        + " km/s",
+        size=13,
+        horizontalalignment="center",
+    )
     plt.show()
-    print("K2: ", Chi2K2min, " +- " , K2err)
-
-
+    print("K2: ", Chi2K2min, " +- ", K2err)
 
 
 ###########################
@@ -115,41 +142,56 @@ if len(K2s) > 1:
 ###########################
 
 if len(K1s) > 1 and len(K2s) > 1:
+    # head = open(fileK1K2).readlines()[0].split()
 
+    MinChis1, MaxChis1, MinChis2, MaxChis2 = (
+        np.amin(chis1),
+        np.amax(chis1),
+        np.amin(chis2),
+        np.amax(chis2),
+    )
 
-
-    #head = open(fileK1K2).readlines()[0].split()
-    
-    MinChis1, MaxChis1, MinChis2, MaxChis2 = np.amin(chis1), np.amax(chis1), np.amin(chis2), np.amax(chis2)
-    
     LimMin = min(MinChis1, MinChis2)
     LimMax = max(MaxChis1, MaxChis2)
-    
-    Chi2K1min, Chi2K2min =  K1s[minind1], K2s[minind2]
 
+    Chi2K1min, Chi2K2min = K1s[minind1], K2s[minind2]
 
     K1min = minind1
 
     fig, ax = plt.subplots()
-    plt.scatter(Chi2K2min, Chi2K1min, color='green')
+    plt.scatter(Chi2K2min, Chi2K1min, color="green")
     X, Y = np.meshgrid(K2s[indK2cut1:indK2cut2], K1s[indK1cut1:indK1cut2])
 
-    cs = plt.contourf(X, Y, Z[indK2cut1:indK2cut2, indK1cut1:indK1cut2], 200, cmap='inferno_r',  vmin=LimMin, vmax=LimMax, zorder=-1)
+    cs = plt.contourf(
+        X,
+        Y,
+        Z[indK2cut1:indK2cut2, indK1cut1:indK1cut2],
+        200,
+        cmap="inferno_r",
+        vmin=LimMin,
+        vmax=LimMax,
+        zorder=-1,
+    )
 
-    cont = ax.contour(X, Y, Z[indK2cut1:indK2cut2, indK1cut1:indK1cut2], [sigma], linewidths=2, linestyles='dashed', colors='green')
+    cont = ax.contour(
+        X,
+        Y,
+        Z[indK2cut1:indK2cut2, indK1cut1:indK1cut2],
+        [sigma],
+        linewidths=2,
+        linestyles="dashed",
+        colors="green",
+    )
 
-    vertices = (cont.collections[0].get_paths()[0].vertices)
-    K2min, K2max = np.amin(vertices[:,0]), np.amax(vertices[:,0])
-    K1min, K1max = np.amin(vertices[:,1]), np.amax(vertices[:,1])
+    vertices = cont.collections[0].get_paths()[0].vertices
+    K2min, K2max = np.amin(vertices[:, 0]), np.amax(vertices[:, 0])
+    K1min, K1max = np.amin(vertices[:, 1]), np.amax(vertices[:, 1])
 
-
-
-
-    plt.xlabel(r'$K_2 [{\rm km}\,{\rm s}^{-1}]$')
-    plt.ylabel(r'$K_1 [{\rm km}\,{\rm s}^{-1}]$')
+    plt.xlabel(r"$K_2 [{\rm km}\,{\rm s}^{-1}]$")
+    plt.ylabel(r"$K_1 [{\rm km}\,{\rm s}^{-1}]$")
     cbar = plt.colorbar()
-    tickchi = np.arange(0.6, 1., 0.01)
-    cbar.ax.set_ylabel(r'$\chi_{\rm reduced}^2$', size=12)
+    tickchi = np.arange(0.6, 1.0, 0.01)
+    cbar.ax.set_ylabel(r"$\chi_{\rm reduced}^2$", size=12)
     plt.title(fileK1K2)
-    plt.savefig(fileK1K2[:-4] + '_2Dmap.pdf', bbox_inches='tight')
+    plt.savefig(fileK1K2[:-4] + "_2Dmap.pdf", bbox_inches="tight")
     plt.show()
--- plot.py
+++ plot.py
@@ -4,7 +4,6 @@
 # Other options exist, see script below...
 
 
-
 import matplotlib.pyplot as plt
 import sys
 import numpy as np
@@ -14,23 +13,24 @@ import sys
 from astropy.table import Table
 import pandas as pd
 
+
 def read_file(infile, col0=0, col1=1):
-    ext = str(infile.split('.')[-1])
+    ext = str(infile.split(".")[-1])
 
     # Check type of input file (fits or ascii) to read in data correctly
-    if (ext == 'fits') or (ext ==  'fit'):
+    if (ext == "fits") or (ext == "fit"):
         wave, flux = read_fits(infile)
 
-    elif (ext == 'gz'):
+    elif ext == "gz":
         wave, flux = read_tlusty(infile)
 
-    elif (ext == 'dat' or ext == 'ascii' or ext == 'txt' or ext == 'nspec'):
+    elif ext == "dat" or ext == "ascii" or ext == "txt" or ext == "nspec":
         wave, flux = read_ascii(infile)
 
-    elif (ext == 'tfits'):
+    elif ext == "tfits":
         wave, flux = read_uvespop(infile)
 
-    elif (ext == 'hfits'):
+    elif ext == "hfits":
         wave, flux = read_hermes_normalized(infile)
 
     else:
@@ -44,43 +44,44 @@ def read_fits(infile):
 
     header = fits.getheader(infile)
 
-    if 'HIERARCH SPECTRUM EXTRACTION' in header:
+    if "HIERARCH SPECTRUM EXTRACTION" in header:
         wave, flux = read_psfSpec(infile)
 
-    elif 'INSTRUME' in header:
-        ins = header['INSTRUME']
-        if (ins == 'MUSE'):
+    elif "INSTRUME" in header:
+        ins = header["INSTRUME"]
+        if ins == "MUSE":
             wave, flux = read_pampelMUSE(infile)
 
-        elif (ins == 'HERMES'):
+        elif ins == "HERMES":
             wave, flux = read_HERMES(infile)
 
-        elif (ins == 'FEROS'):
+        elif ins == "FEROS":
             wave, flux = read_FEROS(infile)
-        elif (ins == 'XSHOOTER'):
+        elif ins == "XSHOOTER":
             wave, flux = read_XSHOOTER(infile)
-        elif (ins == 'UVES'):
+        elif ins == "UVES":
             wave, flux = read_UVES(infile)
-        elif (ins == 'UVES_STITCH'):
-            wave, flux = read_UVES_STITCH(infile)                    
-        elif (ins == 'GIRAFFE' and 'nLR' in infile):
-            wave, flux = read_GIRAFFE(infile)     
-        elif (ins == 'GIRAFFE'):
-            #wave, flux = read_GIRAFFE(infile)               
-            wave, flux = read_GIRAFFE2(infile)               
-        elif (ins == 'ESPCOUDE'):
-            wave, flux = read_NLA(infile)   
-        elif (ins == 'COS'):
-            wave, flux = read_COS(infile)               
-        elif (ins == 'STIS'):
-            wave, flux = read_STIS(infile)              
+        elif ins == "UVES_STITCH":
+            wave, flux = read_UVES_STITCH(infile)
+        elif ins == "GIRAFFE" and "nLR" in infile:
+            wave, flux = read_GIRAFFE(infile)
+        elif ins == "GIRAFFE":
+            # wave, flux = read_GIRAFFE(infile)
+            wave, flux = read_GIRAFFE2(infile)
+        elif ins == "ESPCOUDE":
+            wave, flux = read_NLA(infile)
+        elif ins == "COS":
+            wave, flux = read_COS(infile)
+        elif ins == "STIS":
+            wave, flux = read_STIS(infile)
         else:
-            print('File type unkown, trying HERMES')
-            wave, flux = read_HERMES(infile)                        
+            print("File type unkown, trying HERMES")
+            wave, flux = read_HERMES(infile)
     else:
         wave, flux = read_HERMES(infile)
 
     return wave, flux
+
 
 def read_NLA(infile):
     hdul = fits.open(infile)
@@ -90,20 +91,21 @@ def read_NLA(infile):
     raw = data[1, :, :][0]
     bkg = data[2, :, :][0]
     sigma = data[3, :, :][0]
-    wl0 = header['CRVAL1']
-    delt = header['CD1_1']
-    pix = header['CRPIX1']
+    wl0 = header["CRVAL1"]
+    delt = header["CD1_1"]
+    pix = header["CRPIX1"]
     wave = wl0 - (delt * pix - delt) + np.arange(flux.shape[0]) * delt
     return wave, flux
+
 
 def read_psfSpec(infile):
     print("%s: Input file is a PSF extracted file." % infile)
     header = fits.getheader(infile)
     flux = fits.getdata(infile, ext=0)
     err = fits.getdata(infile, ext=1)
-    wl0 = header['CRVAL1']  # Starting wl at CRPIX1
-    delt = header['CDELT1']  # Stepwidth of wl
-    pix = header['CRPIX1']  # Reference Pixel
+    wl0 = header["CRVAL1"]  # Starting wl at CRPIX1
+    delt = header["CDELT1"]  # Stepwidth of wl
+    pix = header["CRPIX1"]  # Reference Pixel
     wave = wl0 - (delt * pix - delt) + np.arange(flux.shape[0]) * delt
     flux_out = flux, err
 
@@ -114,34 +116,32 @@ def read_pampelMUSE(infile):
     print("%s: Input file is a pampelMUSE file." % infile)
     header = fits.getheader(infile)
     flux = fits.getdata(infile, ext=0)
-    #print flux
+    # print flux
     ##print header['CRVAL1'], header['CDELT1'], header['CRPIX1']
-    #err = fits.getdata(infile, ext=1)
-    wl0 = header['CRVAL1']  # Starting wl at CRPIX1
-    delt = header['CDELT1']  # Stepwidth of wl
-    pix = header['CRPIX1']  # Reference Pixel
-    #print 'hi'
-    #print flux.shape[0]
-    #ddasfasf
+    # err = fits.getdata(infile, ext=1)
+    wl0 = header["CRVAL1"]  # Starting wl at CRPIX1
+    delt = header["CDELT1"]  # Stepwidth of wl
+    pix = header["CRPIX1"]  # Reference Pixel
+    # print 'hi'
+    # print flux.shape[0]
+    # ddasfasf
     wave = wl0 - (delt * pix - delt) + np.arange(flux.shape[0]) * delt
-    #print wave
-    #dsadsf
+    # print wave
+    # dsadsf
     # because pampelmuse gives the wl in m --> convert to A
-    #wave = wave 
-    #flux_out = flux, err
+    # wave = wave
+    # flux_out = flux, err
 
     return wave, flux
-
-
 
 
 def read_MUSE(infile):
     print("%s: Input file is a MUSE file." % infile)
     header = fits.getheader(infile)
     data = fits.getdata(infile)
-    wl0 = header['CRVAL1']  # Starting wl at CRPIX1
-    delt = header['CDELT1']  # Stepwidth of wl
-    pix = header['CRPIX1']  # Reference Pixel
+    wl0 = header["CRVAL1"]  # Starting wl at CRPIX1
+    delt = header["CDELT1"]  # Stepwidth of wl
+    pix = header["CRPIX1"]  # Reference Pixel
     wave = wl0 - (delt * pix - delt) + np.arange(data.shape[0]) * delt
 
     flux = data
@@ -153,50 +153,50 @@ def read_GIRAFFE(infile):
     print("%s: Input file is a GIRAFFE file." % infile)
     header = fits.getheader(infile)
     data = fits.getdata(infile)
-    wl0 = header['CRVAL1']  # Starting wl at CRPIX1
-    delt = header['CDELT1']  # Stepwidth of wl
-    pix = header['CRPIX1']  # Reference Pixel
+    wl0 = header["CRVAL1"]  # Starting wl at CRPIX1
+    delt = header["CDELT1"]  # Stepwidth of wl
+    pix = header["CRPIX1"]  # Reference Pixel
     wave = wl0 - (delt * pix - delt) + np.arange(data.shape[0]) * delt
     table = Table.read(infile, hdu=1)
-    flux = table['NORM_SKY_SUB_CR']
+    flux = table["NORM_SKY_SUB_CR"]
     return wave, flux
+
 
 def read_GIRAFFE2(infile):
     print("%s: Input file is a GIRAFFE2 file." % infile)
-    hdunum=1
+    hdunum = 1
     header = fits.getheader(infile)
     hdul = fits.open(infile)
     table = Table.read(infile, hdu=hdunum)
-    wave = table['WAVE'][0]*10.
-    flux = table['FLUX_REDUCED'][0]    
+    wave = table["WAVE"][0] * 10.0
+    flux = table["FLUX_REDUCED"][0]
     return wave, flux
-
 
 
 def read_HERMES(infile):
     print("%s: Input file is a HERMES file." % infile)
     header = fits.getheader(infile)
     # for files with standard wavelegth array
-    if ((header['CTYPE1'] == 'WAVELENGTH') or (header['CTYPE1'] == 'AWAV')):
+    if (header["CTYPE1"] == "WAVELENGTH") or (header["CTYPE1"] == "AWAV"):
         flux = fits.getdata(infile)
-        crval = header['CRVAL1']
-        cdelt = header['CDELT1']
-        naxis1 = header['NAXIS1']
+        crval = header["CRVAL1"]
+        cdelt = header["CDELT1"]
+        naxis1 = header["NAXIS1"]
         wave = crval + np.arange(0, naxis1) * cdelt
 
     # for files that are given in logarithmic wl array
-    if (header['CTYPE1'] == 'log(wavelength)'):
+    if header["CTYPE1"] == "log(wavelength)":
         flux = fits.getdata(infile)
-        crval = header['CRVAL1']
-        cdelt = header['CDELT1']
-        naxis1 = header['NAXIS1']
-        wave = np.exp(crval + np.arange(0, naxis1)*cdelt)
+        crval = header["CRVAL1"]
+        cdelt = header["CDELT1"]
+        naxis1 = header["NAXIS1"]
+        wave = np.exp(crval + np.arange(0, naxis1) * cdelt)
     else:
         flux = fits.getdata(infile)
-        crval = header['CRVAL1']
-        cdelt = header['CDELT1']
-        naxis1 = header['NAXIS1']
-        wave = crval + np.arange(0, naxis1) * cdelt                
+        crval = header["CRVAL1"]
+        cdelt = header["CDELT1"]
+        naxis1 = header["NAXIS1"]
+        wave = crval + np.arange(0, naxis1) * cdelt
     return wave, flux
 
 
@@ -204,9 +204,9 @@ def read_FEROS(infile):
     print("%s: Input file is a FEROS file." % infile)
     header = fits.getheader(infile)
     flux = fits.getdata(infile)
-    crval = header['CRVAL1']
-    crpix = header['CRPIX1']
-    cdelt = header['CDELT1']
+    crval = header["CRVAL1"]
+    crpix = header["CRPIX1"]
+    cdelt = header["CDELT1"]
 
     wave = crval - (cdelt * crpix - cdelt) + np.arange(flux.shape[0]) * cdelt
     return wave, flux
@@ -217,15 +217,15 @@ def read_XSHOOTER(infile):
     header = fits.getheader(infile)
     hdul = fits.open(infile)
     table = Table.read(infile, hdu=1)
-    wave = table['WAVE'][0]
-    #flux = table['FLUX_REDUCED'][0]
-    flux = table['FLUX'][0]    
-    #else:
-        #flux = fits.getdata(infile)
-        #crval = header['CRVAL1']
-        #cdelt = header['CDELT1']
-        #naxis1 = header['NAXIS1']
-        #wave = crval + np.arange(0, naxis1) * cdelt
+    wave = table["WAVE"][0]
+    # flux = table['FLUX_REDUCED'][0]
+    flux = table["FLUX"][0]
+    # else:
+    # flux = fits.getdata(infile)
+    # crval = header['CRVAL1']
+    # cdelt = header['CDELT1']
+    # naxis1 = header['NAXIS1']
+    # wave = crval + np.arange(0, naxis1) * cdelt
     return wave, flux
 
 
@@ -234,97 +234,99 @@ def read_COS(infile):
     header = fits.getheader(infile)
     hdul = fits.open(infile)
     table = Table.read(infile, hdu=1)
-    waves = table['WAVELENGTH']
-    fluxes = table['FLUX'] 
+    waves = table["WAVELENGTH"]
+    fluxes = table["FLUX"]
     wave = []
     flux = []
-    for jj in np.arange(len(waves)-1, 0, -1):
-        #print jj
+    for jj in np.arange(len(waves) - 1, 0, -1):
+        # print jj
         for w in waves[jj]:
             wave.append(w)
         for f in fluxes[jj]:
-            flux.append(f)  
-    #flux = table['FLUX_REDUCED'][0]
-    #flux = table['FLUX'] 
-    #print wave
-    #print flux
-    #dsadd
-    #else:
-        #flux = fits.getdata(infile)
-        #crval = header['CRVAL1']
-        #cdelt = header['CDELT1']
-        #naxis1 = header['NAXIS1']
-        #wave = crval + np.arange(0, naxis1) * cdelt
+            flux.append(f)
+    # flux = table['FLUX_REDUCED'][0]
+    # flux = table['FLUX']
+    # print wave
+    # print flux
+    # dsadd
+    # else:
+    # flux = fits.getdata(infile)
+    # crval = header['CRVAL1']
+    # cdelt = header['CDELT1']
+    # naxis1 = header['NAXIS1']
+    # wave = crval + np.arange(0, naxis1) * cdelt
     return wave, flux
+
 
 def read_STIS(infile):
     print("%s: Input file is a STIS file." % infile)
     header = fits.getheader(infile)
     hdul = fits.open(infile)
     table = Table.read(infile, hdu=1)
-    #print table
-    wave = table['WAVELENGTH'][0]
-    flux = table['FLUX'][0]
-    #print waves
-    #print fluxes
-    #wave = []
-    #flux = []
-    #print 'hello'
-    #print np.arange(len(waves[0])-1, 0, -1)
-    #for jj in np.arange(len(waves[0])-1, 0, -1):        
-        #print jj, waves[0][jj], fluxes[jj]
-        #for w in waves[jj]:
-            #wave.append(w)
-        #for f in fluxes[jj]:
-            #flux.append(f)  
-    #flux = table['FLUX_REDUCED'][0]
-    #flux = table['FLUX'] 
-    #print wave
-    #print flux
-    #dsadd
-    #else:
-        #flux = fits.getdata(infile)
-        #crval = header['CRVAL1']
-        #cdelt = header['CDELT1']
-        #naxis1 = header['NAXIS1']
-        #wave = crval + np.arange(0, naxis1) * cdelt
+    # print table
+    wave = table["WAVELENGTH"][0]
+    flux = table["FLUX"][0]
+    # print waves
+    # print fluxes
+    # wave = []
+    # flux = []
+    # print 'hello'
+    # print np.arange(len(waves[0])-1, 0, -1)
+    # for jj in np.arange(len(waves[0])-1, 0, -1):
+    # print jj, waves[0][jj], fluxes[jj]
+    # for w in waves[jj]:
+    # wave.append(w)
+    # for f in fluxes[jj]:
+    # flux.append(f)
+    # flux = table['FLUX_REDUCED'][0]
+    # flux = table['FLUX']
+    # print wave
+    # print flux
+    # dsadd
+    # else:
+    # flux = fits.getdata(infile)
+    # crval = header['CRVAL1']
+    # cdelt = header['CDELT1']
+    # naxis1 = header['NAXIS1']
+    # wave = crval + np.arange(0, naxis1) * cdelt
     return wave, flux
 
 
 def read_UVES(infile):
     print("%s: Input file is a UVES or GIRAFFE file." % infile)
     header = fits.getheader(infile)
-    if 'SPEC_COM' in header:
-        print('a')
+    if "SPEC_COM" in header:
+        print("a")
         table = Table.read(infile, hdu=1)
-        wave = table['wave']
-        flux = table['FLUX_REDUCED']
-    elif 'CRVAL1' in header:
-        print('b')
+        wave = table["wave"]
+        flux = table["FLUX_REDUCED"]
+    elif "CRVAL1" in header:
+        print("b")
         flux = fits.getdata(infile)
-        crval = header['CRVAL1']
-        cdelt = header['CDELT1']
-        naxis1 = header['NAXIS1']
+        crval = header["CRVAL1"]
+        cdelt = header["CDELT1"]
+        naxis1 = header["NAXIS1"]
         wave = crval + np.arange(0, naxis1) * cdelt
     else:
-        print('c')
+        print("c")
         table = Table.read(infile, hdu=0)
-        wave = table['WAVE'][0]
-        flux = table['FLUX'][0]
+        wave = table["WAVE"][0]
+        flux = table["FLUX"][0]
     if wave[0] < 1000:  # wavelength is given in nm
-        wave = wave * 10 # convert wavelength to A
+        wave = wave * 10  # convert wavelength to A
     return wave, flux
+
 
 def read_UVES_STITCH(infile):
     print("%s: Input file is a stitched UVES file" % infile)
     table = Table.read(infile, hdu=1)
-    wave = table['WAVE']
-    flux = table['FLUX']
+    wave = table["WAVE"]
+    flux = table["FLUX"]
     return wave, flux
 
 
 def read_tlusty(infile):
-    fill_val = 'extrapolate'
+    fill_val = "extrapolate"
     print("%s: Input file is a TLUSTY model." % infile)
 
     # first the model
@@ -335,7 +337,7 @@ def read_tlusty(infile):
     flux = s(wave)
 
     # now the continuum
-    contfile = infile.split('.7.')[0] + '.17.gz'
+    contfile = infile.split(".7.")[0] + ".17.gz"
     wave_cont, flux_cont = np.loadtxt(contfile, unpack=True)
 
     # wave array not evenly spaced => interpolate it
@@ -372,23 +374,25 @@ def read_ascii(infile, col0=0, col1=1):
     # any type of ascii file (typically I call them .dat)
     # assumes that first column is wave and second column is flux
     print("%s: Input file is an ascii file." % infile)
-    #wave, flux = np.loadtxt(infile, usecols=(0, 1), unpack=True)
-    spec = (pd.read_csv(infile, header=None, delim_whitespace=True, comment='#')).values
-    wave = spec[:,col0]
-    flux = spec[:,col1]
+    # wave, flux = np.loadtxt(infile, usecols=(0, 1), unpack=True)
+    spec = (
+        pd.read_csv(infile, header=None, delim_whitespace=True, comment="#")
+    ).values
+    wave = spec[:, col0]
+    flux = spec[:, col1]
     return wave, flux
 
 
 def cut_muse_specrange(wave, flux):
     # cut the spectrum to MUSE wavelength
-    spec_range = ((wave > 4600) & (wave < 9350))
+    spec_range = (wave > 4600) & (wave < 9350)
     wave = wave[spec_range]
     flux = flux[spec_range]
     return wave, flux
 
 
 def mask_muse_laser(wave, flux):
-    index = [j for j in range(len(wave)) if 5746.0 < wave[j] < 6017.]
+    index = [j for j in range(len(wave)) if 5746.0 < wave[j] < 6017.0]
     flux[index] = np.nan
 
     return wave, flux
@@ -419,7 +423,6 @@ def write_hermes(infile, flux_cont, outfile):
 
 
 def write_extracted_spectrum(header, flux, flux_err, outfilename):
-
     hdul_new = fits.HDUList()
     hdul_new.append(fits.PrimaryHDU(data=flux, header=header))
     hdul_new.append(fits.ImageHDU(data=flux_err))
@@ -434,7 +437,6 @@ def write_2Dimage(header, image, outfilename):
     hdul_new.writeto(outfilename)
 
 
-
 fig, ax = plt.subplots()
 
 Legend = False
@@ -442,43 +444,43 @@ Binning = False
 ScatterErr = False
 Skip = False
 SkipTwice = False
-Norm=False
-for i in range(len(sys.argv)-1):
+Norm = False
+for i in range(len(sys.argv) - 1):
     print(i)
     if Skip:
         if SkipTwice:
-            Skip=True
-            SkipTwice=False
+            Skip = True
+            SkipTwice = False
         else:
-            Skip=False
+            Skip = False
         continue
-    j = i+1
-    if len(sys.argv) >1:
-        if sys.argv[j] == '--legend':
-            Legend = True   
+    j = i + 1
+    if len(sys.argv) > 1:
+        if sys.argv[j] == "--legend":
+            Legend = True
             continue
-        if sys.argv[j] == '--norm':
-            Norm = True   
-            continue    
-        if sys.argv[j] == '--scatterr':
-            ScatterErr = True   
-            continue     
-        if sys.argv[j] == '--cols':
-            col0 = float(sys.argv[j+1])
-            col1 = float(sys.argv[j+2])         
+        if sys.argv[j] == "--norm":
+            Norm = True
+            continue
+        if sys.argv[j] == "--scatterr":
+            ScatterErr = True
+            continue
+        if sys.argv[j] == "--cols":
+            col0 = float(sys.argv[j + 1])
+            col1 = float(sys.argv[j + 2])
             Skip = True
             SkipTwice = True
-            continue       
+            continue
     infile = sys.argv[j]
-    try:       
+    try:
         try:
             wave_in, flux_in = read_file(infile, col0, col1)
         except:
             wave_in, flux_in = read_file(infile)
-        flux_in = np.nan_to_num(flux_in, 1.)
+        flux_in = np.nan_to_num(flux_in, 1.0)
     except:
         continue
-    try:        
+    try:
         wave_in = wave_in.astype(float)
         flux_in = flux_in.astype(float)
     except:
@@ -490,15 +492,23 @@ for i in range(len(sys.argv)-1):
         flux = flux_in
     if Norm:
         flux /= np.mean(flux)
-    # Do the plotting    
-    name = str(infile).split('.fits')[0]
+    # Do the plotting
+    name = str(infile).split(".fits")[0]
     if ScatterErr:
-        ax.errorbar(wave_in, flux, yerr=np.loadtxt(infile)[:,2], fmt='o', linewidth=1.0, alpha=0.8, label=name)
+        ax.errorbar(
+            wave_in,
+            flux,
+            yerr=np.loadtxt(infile)[:, 2],
+            fmt="o",
+            linewidth=1.0,
+            alpha=0.8,
+            label=name,
+        )
     else:
-        ax.plot(wave_in, flux, linewidth=1.0, alpha=0.8, label=name)    
+        ax.plot(wave_in, flux, linewidth=1.0, alpha=0.8, label=name)
 if Legend:
     ax.legend()
 ax.set_xlabel("Wavelength [A]")
 ax.set_ylabel("Flux")
 plt.show()
-#np.savetxt(infile + '.txt', np.c_[wave_in, flux])
+# np.savetxt(infile + '.txt', np.c_[wave_in, flux])
